c832f8564172792715cf00af7b33b7f712f49594
GVOne-blood <dta150904@gmail.com>
2023-08-15 16:20:16 +0700

    btvn buoi 3

--- BTVN_B3/bai4.java
+++ BTVN_B3/bai4.java
@@ -1,30 +1,31 @@
 import java.util.Scanner;
 
-public class Bai5 {
+public class bai5 {
     public static void main(String[] args) {
         Scanner nhap = new Scanner(System.in);
-        String[] a = new String[6];
-        String[] b = new String[6];
+        String[] a = new String[3];
+        String[] b = new String[3];
         int dem = 0;
         int k = 0;
         do {
             a[k] = nhap.next();
             b[k] = a[k];
+          // System.out.println(a[k]);
+            k++;// System.out.println(k);
             dem++;
-            k++;
-        } while (!a[k - 1].isEmpty() && k < 6);
-
-        for (int i = 0; i < dem; i++) {
+        } 
+        while (a[k-1].isEmpty()&&  k<4);
+        for (int i = 0; i < 3; i++) {
             a[i] = a[i].toLowerCase();
             b[i] = b[i].toUpperCase();
             a[i] = a[i].replace(a[i].charAt(0), b[i].charAt(0));
-            if (i == dem - 1) {
+            if (i == 2) {
                 System.out.print(a[i]);
             } else {
                 System.out.print((a[i] + " "));
             }
         }
 
-        nhap.close();
+        //dnhap.close();
     }
 }
